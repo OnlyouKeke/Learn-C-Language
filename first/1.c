cfb252575e2c0aa9b27b73154fdd740c7aa4e0f8
Super User <root@localhost.localdomain>
2024-11-24 21:01:57 +0800

    first day

---
+++ first/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    int age = 19;      // int 数值型
+    float gpa = 2.05;   // float浮点数
+    char grade = 'C';   // char字符型
+    char name[] = "Bro"; // 字符型 的赋值
+
+
+    printf("hello %s\n",name);
+    printf("you are %d years old \n",age);
+    printf("Your averge grade is %c\n",grade);
+    printf("Your gpa is %f\n")
+    printf("xs,yyds");
+    return 0;
+}
